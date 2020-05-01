d30d7a21c7950696cbb8c55de62c62db6b0138ab
Akshay Shete <akshay.shete@ad.infosys.com>
2020-05-01 21:49:03 +0530

    Spring GET Call

--- src/main/java/com/akshay/demo/Controller/LoginController.java
+++ src/main/java/com/akshay/demo/Controller/LoginController.java
@@ -1,6 +1,10 @@
 package com.akshay.demo.Controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +18,13 @@ public class LoginController
 {
 	@Autowired
 	LoginServices loginservices;
+	
+	
+	@GetMapping(path="/api/getData/{id}")
+	public Optional<LoginEntity> getDetailsOfParticularID(@PathVariable String id)
+	{
+		return loginservices.getEntity(id);
+	}
 	
 	@PostMapping(path="/api/postData", consumes="application/json", produces="application/json" )
 	public LoginEntity getDetails(@RequestBody LoginDTO logindto)
--- src/main/java/com/akshay/demo/Services/LoginServices.java
+++ src/main/java/com/akshay/demo/Services/LoginServices.java
@@ -1,5 +1,7 @@
 package com.akshay.demo.Services;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -20,5 +22,10 @@ public class LoginServices
 		loginEntity.setPassword(logindto.getPassword());
 		
 		return loginrepo.save(loginEntity);
+	}
+	
+	public Optional<LoginEntity> getEntity(String id)
+	{
+		return loginrepo.findById(id);
 	}
 }
